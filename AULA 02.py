30dc8446cfde72147108b9879d6df081d686e982
Guilherme belarminio <belarmino.contatoo2@gmail.com>
2023-12-07 22:23:22 -0300

    INDEX E VARIÁVEL.MÉTODO()

---
+++ AULA 02.py
@@ -0,0 +1,24 @@
+# AULA SOBRE INDEX E VARIÁVEL.MÉTODO()
+
+mensagem = str('eu amo comer melancia')
+
+print(mensagem.upper()) #nesse você consegue deixar todo em maiúsculo
+
+print(mensagem.lower()) #nesse você consegue deixar todo em minúsculo
+
+print(mensagem.capitalize()) #nesse você consegue deixar a primeira palavra em maiúsculo
+
+print(mensagem.replace('a','4')) #com esse você consegue trocar as letras ou o que vc determinar por outras coisa.
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Aula 1.py
+++ Aula 1.py
@@ -1,14 +1,20 @@
-
-ano_nascimento = input(' em que ano você nasceu ?')
-idade = 2023 - int(ano_nascimento)
-
-print(idade)
+# QUESTIONÁRIO SIMPLES DE PERGUNTAS E RESPOSTAS!!
 
 
+print('Olá, vamos começar ? irei iniciar o questionário com algumas perguntas básicas sobre você. :D')
 
+nome = input('qual o seu nome ?')
+sobrenome = input('Agora me fale seu sobrenome:')
+aperlido = input('Como seus amigos te chamam?')
+idade = input('qual a sua idade ?')
+e_mail = input('Digite o seu E-mail ?')
+desejo = input('Qual o seu maior desejo?')
 
+texto0 = f'você se chama {nome} {sobrenome} e tem {idade} anos e seu melhor E-mail é {e_mail}, seu maior desejo é {desejo}, {nome} vamos continuar!'
+print(texto0)
 
+texto1 = f'já que você tem {idade} anos, então você nasceu no ano de '
+idade = int(2023) - int(idade)
+print(f' {texto1}{idade}')
 
-
-
-
+print(f'obrigado por me falar um pouco sobre você, até maiS {aperlido} :D.')
